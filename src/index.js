23dd7468a4572ee4368092d54b4171b517f2594c
BeIchTuan <22521595@gm.uit.edu.vn>
2024-10-14 10:43:13 +0700

    conect mongodb

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const morgan = require('morgan');
+const { MongoClient } = require('mongodb');
 const app = express()
 
 dotenv.config()
@@ -10,6 +11,37 @@ const port = process.env.PORT || 3001
 app.get('/', (req, res) => {
   res.send('Hello world!')
 })
+
+async function main(){
+  /**
+   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
+   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
+   */
+  const uri = "mongodb+srv://hoangvanluong:hvl123456@ecommerce.ko8pa.mongodb.net/sample_airbnb?retryWrites=true&w=majority";
+
+
+  const client = new MongoClient(uri);
+
+  try {
+      // Connect to the MongoDB cluster
+      await client.connect();
+
+      // Make the appropriate DB calls
+      await  listDatabases(client);
+
+  } catch (e) {
+      console.error(e);
+  } finally {
+      await client.close();
+  }
+}
+
+main().catch(console.error);
+async function listDatabases(client){
+  databasesList = await client.db().admin().listDatabases();
+  console.log("Databases:");
+  databasesList.databases.forEach(db => console.log(` - ${db.name}`));
+};
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
