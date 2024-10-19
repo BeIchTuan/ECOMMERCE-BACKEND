cc132b32b435b2f2e2f7696c3fa5ca97d52907ad
BeIchTuan <22521595@gm.uit.edu.vn>
2024-10-19 18:33:18 +0700

    add authorize

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,37 @@
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+// Middleware xác thực và phân quyền
+const authMiddleware = (allowedRoles = []) => {
+    return async (req, res, next) => {
+        const { accessToken } = req.cookies;
+
+        if (!accessToken) {
+            return res.status(409).json({ error: 'Please Login First' });
+        } else {
+            try {
+                // Giải mã token
+                const deCodeToken = await jwt.verify(accessToken, process.env.ACCESS_TOKEN);
+
+                // Lưu thông tin người dùng vào request
+                req.role = deCodeToken.role;
+                req.id = deCodeToken.id;
+
+                // Kiểm tra xem role của user có được phép truy cập không
+                if (allowedRoles.length && !allowedRoles.includes(req.role)) {
+                    return res.status(403).json({ error: 'Access denied: Insufficient permissions' });
+                }
+
+                next();
+            } catch (error) {
+                return res.status(409).json({ error: 'Please Login' });
+            }
+        }
+    };
+};
+
+module.exports = {
+    authMiddleware
+}
