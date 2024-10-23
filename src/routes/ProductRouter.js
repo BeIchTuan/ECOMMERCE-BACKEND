20f49a88904bb5650a8745f8af35dc50557e79e7
BeIchTuan <22521595@gm.uit.edu.vn>
2024-10-23 22:29:23 +0700

    add router get all shop products

--- src/routes/ProductRouter.js
+++ src/routes/ProductRouter.js
@@ -6,5 +6,6 @@ const { authMiddleware } = require('../middlewares/authMiddleware');
 router.post('/products', authMiddleware(['seller']),productController.createProduct);
 router.put('/products/:id', authMiddleware(['seller']),productController.updateProduct);
 router.delete('/products/:id', authMiddleware(['seller']),productController.deleteProduct);
+router.get('/products/:sellerId', authMiddleware(['seller', 'user']),productController.getAllShopProduct);
 
 module.exports = router
