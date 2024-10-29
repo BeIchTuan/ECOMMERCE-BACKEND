d6dd037c410a721d16e7ea849677e4070105461d
BeIchTuan <22521595@gm.uit.edu.vn>
2024-10-29 23:15:56 +0700

    add discount attribute add thumbnail

--- src/models/ProductModel.js
+++ src/models/ProductModel.js
@@ -19,11 +19,20 @@ const productSchema = new Schema({
     category: [{ type: Schema.Types.ObjectId, ref: 'Categories',  required: true }],
     inStock: { type: Number, required: true },
     image: [{ type: String,  required: true }],
-    //discount: { type: Schema.Types.ObjectId, ref: 'Discount' },
+    discount: { type: Schema.Types.ObjectId, ref: 'Discount' },
     //rates: [{ type: Schema.Types.ObjectId, ref: 'Rate' }],
     rate: { type: Number, default: 0 },
     seller: { type: Schema.Types.ObjectId, ref: 'User'},
 });
+
+// Add a virtual for `thumbnail` that returns the first image in the `image` array
+productSchema.virtual('thumbnail').get(function() {
+  return this.image.length > 0 ? this.image[0] : null;
+});
+
+// Ensure virtuals are included in JSON responses
+productSchema.set('toJSON', { virtuals: true });
+productSchema.set('toObject', { virtuals: true });
 
 const Product = mongoose.model("Product", productSchema);
 
