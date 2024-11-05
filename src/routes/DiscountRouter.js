89edf2bbfdb8a6de51e666758267962dae9332da
BeIchTuan <22521595@gm.uit.edu.vn>
2024-11-05 15:13:58 +0700

    change router get discount

--- src/routes/DiscountRouter.js
+++ src/routes/DiscountRouter.js
@@ -4,7 +4,7 @@ const discountController = require('../controllers/DiscountController');
 const { authMiddleware } = require('../middlewares/authMiddleware');
 
 router.post('/seller/discounts', authMiddleware(['seller']), discountController.createDiscount);
-router.get('/seller/discounts/:sellerId', discountController.getAllDiscounts);
+router.get('/discounts/:sellerId', discountController.getAllDiscounts);
 //router.get('/:id', discountController.getDiscountById);
 router.put('/seller/discounts/:discountId', authMiddleware(['seller']), discountController.updateDiscount);
 router.delete('/seller/discounts/:discountId',authMiddleware(['seller']), discountController.deleteDiscount);
